121c211d5d9190549f5135c0adf252f72f9ed8f0
Vitaly Klochkov <klochkov.v.a@gmail.com>
2024-01-27 20:05:55 +0300

    page-product 0

--- storbriz/shop/urls.py
+++ storbriz/shop/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import GoodsList
+from .views import GoodsList, GoodDetailView
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', GoodsList.as_view(), name='home'),
+    path('good/<int:pk>', GoodDetailView.as_view(), name='good'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storbriz/shop/views.py
+++ storbriz/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Good
 
@@ -14,3 +14,8 @@ class GoodsList(ListView):
                     'top_goods': Good.objects.filter(is_popular=True)}
         return queryset
 
+
+class GoodDetailView(DetailView):
+    model = Good
+    template_name = 'src/page-product.html'
+    context_object_name = 'good'
