c78adf2a49ea3298ce77f80186e1b751a9e444cb
Vitaly Klochkov <klochkov.v.a@gmail.com>
2024-01-28 19:24:40 +0300

    cart link done

--- storbriz/shop/urls.py
+++ storbriz/shop/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import GoodsList, GoodDetailView
+from .views import GoodsList, GoodDetailView, CartDetailView
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', GoodsList.as_view(), name='home'),
     path('good/<int:pk>', GoodDetailView.as_view(), name='good'),
+    path('<int:pk>/cart', CartDetailView.as_view(), name='cart')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storbriz/shop/views.py
+++ storbriz/shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Good
+from .models import Good, Cart
 
 
 class GoodsList(ListView):
@@ -19,3 +19,9 @@ class GoodDetailView(DetailView):
     model = Good
     template_name = 'src/page-product.html'
     context_object_name = 'good'
+
+
+class CartDetailView(DetailView):
+    model = Cart
+    template_name = 'src/basket.html'
+    context_object_name = 'cart'
