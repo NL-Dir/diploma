1576e0728df9c4f459e8a5ae4a20cd3c2b2d8551
Vitaly Klochkov <klochkov.v.a@gmail.com>
2024-01-29 21:29:20 +0300

    add_to_cart done

--- storbriz/shop/urls.py
+++ storbriz/shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import GoodsList, GoodDetailView, CartDetailView, delete_from_cart
+from .views import GoodsList, GoodDetailView, CartDetailView, delete_from_cart, add_to_cart
 
 app_name = 'shop'
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('good/<int:pk>', GoodDetailView.as_view(), name='good'),
     path('<int:pk>/cart', CartDetailView.as_view(), name='cart'),
     path('<int:pk>/cart/<int:good_id>/delete', delete_from_cart, name='delete_from_cart'),
+    path('<int:pk>/cart/<int:good_id>/add', add_to_cart, name='add_to_cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storbriz/shop/views.py
+++ storbriz/shop/views.py
@@ -13,8 +13,8 @@ class GoodsList(ListView):
     context_object_name = 'goods'
 
     def get_queryset(self):
-        queryset = {'new_goods': Good.objects.order_by('is_new', 'id'),
-                    'top_goods': Good.objects.filter(is_popular=True)}
+        queryset = {'new_goods': Good.objects.order_by('-is_new', '-id')[:12],
+                    'top_goods': Good.objects.filter(is_popular=True)[:12]}
         return queryset
 
 
@@ -23,26 +23,26 @@ class GoodDetailView(DetailView):
     template_name = 'src/page-product.html'
     context_object_name = 'good'
 
+    def get_success_usl(self):
+        if self.request.POST:
+            print(self.request.user)
+            return reverse_lazy('shop:good',
+                                kwargs={'pk': self.id})
+
 
 class CartDetailView(DetailView):
     model = Cart
     template_name = 'src/basket.html'
     context_object_name = 'cart'
 
-    def get_success_url(self, request):
-        if 'delete_from_cart' in self.request.POST:
-            request.user.cart.goods.remove(request.POST['good_id'])
-            return reverse_lazy('shop:cart',
-                                kwargs={'pk': self.id,
-                                        })
-        else:
-            return reverse_lazy('shop:cart',
-                                kwargs={'pk': self.id,
-                                        })
+
+def add_to_cart(request, pk, good_id):
+    request.user.cart.goods.add(Good.objects.get(id=good_id))
+    request.user.cart.save()
+    return HttpResponseRedirect(reverse('shop:good', kwargs={'pk': good_id}))
 
 
 def delete_from_cart(request, pk, good_id):
-    if request.method == "GET":
-        request.user.cart.goods.remove(Good.objects.get(id=good_id))
-        request.user.cart.save()
+    request.user.cart.goods.remove(Good.objects.get(id=good_id))
+    request.user.cart.save()
     return HttpResponseRedirect(reverse('shop:cart', kwargs={'pk': pk}))
